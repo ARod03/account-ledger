62b937c0360c6736f52cee9f419a621818367f65
adrianrodriguez <ardgz2003@gmail.com>
2026-04-29 09:16:31 -0500

    Created a main menu.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner theScanner = new Scanner(System.in);
+        boolean appRunning = true;
+
+        //Create a while loop to efficiently navigate through the menu
+        while(appRunning){
+            System.out.println("Welcome to the main menu!");
+            System.out.println("What would you like to do?: ");
+            System.out.println("----------");
+            System.out.println("D) Add a deposit");
+            System.out.println("P) Make Payment (Debit)");
+            System.out.println("L) Ledger");
+            System.out.println("X) Quit");
+            System.out.println("----------");
+
+
+        }
+
+
+
+    }
+
 }
